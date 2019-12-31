e6086e6c1af432a4583a0bbcf3d37b139f4417ee
Lyon <505242941@qq.com>
2019-12-31 13:32:26 +0800

    图片demo1

---
+++ ImagesIssue/Readme.md
@@ -0,0 +1,22 @@
+﻿# docker 或linux 中使用 图片类
+
+>一. 引用ZKWeb.System.Drawing
+`Install-Package ZKWeb.System.Drawing`
+
+>二. 页面上引用using System.DrawingCore
+将原来的
+using System.Drawing;
+using System.Drawing.Imaging;
+改为ZKWeb的 
+using System.DrawingCore;
+using System.DrawingCore.Imaging;
+
+>三. 修改DockerFile
+
+3.1 加入安装命令
+`RUN apt-get update -y && apt-get install -y libgdiplus && apt-get clean && ln -s /usr/lib/libgdiplus.so /usr/lib/gdiplus.dll`
+3.2 或直接用 stulzq/dotnet:2.2.0-aspnetcore-runtime-with-image 做为底包
+
+将 FROM mcr.microsoft.com/dotnet/core/aspnet:2.2-stretch-slim AS base
+改为
+FROM stulzq/dotnet:2.2.0-aspnetcore-runtime-with-image AS base
---
+++ ImagesIssue/src/Controllers/ValuesController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.DrawingCore;
+using System.DrawingCore.Imaging;
+using System.IO;
+
+namespace ImagesIssue.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public FileResult Get()
+        {
+            string[] chkCode = new string[] { "1", "1", "1", "1", "1", "1" };
+            int codeW = 80;
+            int codeH = 30;
+            int fontSize = 16;
+            Random rnd = new Random();
+            //颜色列表，用于验证码、噪线、噪点 
+            Color[] color = { Color.Black, Color.Red, Color.Blue, Color.Green, Color.Orange, Color.Brown, Color.Brown, Color.DarkBlue };
+            //字体列表，用于验证码 
+            string[] font = { "Times New Roman" };
+            //验证码的字符集，去掉了一些容易混淆的字符 
+            //写入Session、验证码加密
+            //WebHelper.WriteSession("session_verifycode", Md5Helper.MD5(chkCode.ToLower(), 16));
+            //创建画布
+            Bitmap bmp = new Bitmap(codeW, codeH);
+            Graphics g = Graphics.FromImage(bmp);
+            g.Clear(Color.White);
+            //画噪线 
+            for (int i = 0; i < 1; i++)
+            {
+                int x1 = rnd.Next(codeW);
+                int y1 = rnd.Next(codeH);
+                int x2 = rnd.Next(codeW);
+                int y2 = rnd.Next(codeH);
+                Color clr = color[rnd.Next(color.Length)];
+                g.DrawLine(new Pen(clr), x1, y1, x2, y2);
+            }
+            //画验证码字符串 
+            for (int i = 0; i < chkCode.Length; i++)
+            {
+                string fnt = font[rnd.Next(font.Length)];
+                Font ft = new Font(fnt, fontSize);
+                Color clr = color[rnd.Next(color.Length)];
+                g.DrawString(chkCode[i].ToString(), ft, new SolidBrush(clr), (float)i * 18, (float)0);
+            }
+            //将验证码图片写入内存流，并将其以 "image/Png" 格式输出 
+
+
+            return File(Bitmap2Byte(bmp), "image/png", "hello-dotnetcore.png");
+
+        }
+
+        public static byte[] Bitmap2Byte(Bitmap bitmap)
+        {
+            using (MemoryStream stream = new MemoryStream())
+            {
+                bitmap.Save(stream, ImageFormat.Jpeg);
+                byte[] data = new byte[stream.Length];
+                stream.Seek(0, SeekOrigin.Begin);
+                stream.Read(data, 0, Convert.ToInt32(stream.Length));
+                return data;
+            }
+        }
+    }
+}
